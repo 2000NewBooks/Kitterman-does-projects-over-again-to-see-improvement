ed832d72a07cbd46399afd29115f76a05767c182
Andrew Kitterman <taupitheta@gmail.com>
2016-05-17 14:20:56 -0600

    Did some stuff so people aren't idiots.

---
+++ magpie6.java
@@ -0,0 +1,117 @@
+/**
+ * Project 6 VERSION
+ *You need to rename to Magpie.Java!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * A program to carry on conversations with a human user.
+ * This is the initial version that:  
+ * <ul><li>
+ *       Uses indexOf to find strings
+ * </li><li>
+ * 		    Handles responding to simple words and phrases 
+ * </li></ul>
+ * This version uses a nested if to handle default responses.
+ * @author Laurie White
+ * @version April 2012
+ */
+public class Magpie
+{
+	/**
+	 * Get a default greeting 	
+	 * @return a greeting
+	 */
+	public String getGreeting()
+	{
+		return "Hello, let's talk.";
+	}
+	
+	/**
+	 * Gives a response to a user statement
+	 * 
+	 * @param statement
+	 *            the user statement
+	 * @return a response based on the rules given
+	 */
+	public String getResponse(String statement)
+	{
+		String response = "";
+		if (statement.indexOf("no") >= 0)
+		{
+			response = "Why so negative?";
+		}
+		else if (statement.indexOf("mother") >= 0
+				|| statement.indexOf("father") >= 0
+				|| statement.indexOf("sister") >= 0
+				|| statement.indexOf("brother") >= 0)
+		{
+			response = "Tell me more about your family.";
+		}
+		else if(statement.indexOf("dog") >= 0 || statement.indexOf("cat") >= 0)
+		{
+			response = "Tell me more about your pets.";
+		}
+		else if (statement.indexOf("Schreiber") >= 0)
+		{
+			response = "That n00b doesn't look at the SR about him";
+		}
+		else if (statement.trim().length == 0)
+		{
+			response = "nice white space";
+			
+		}
+		else if (statement.indexOf("Dank memes Dude") >= 0)
+		{
+			response = "Mine are the dankest.";
+		}
+		else if (statement.indexOf("AP Comp Sci Rocks") >= 0)
+		{
+			reponse = "this is not the AP exam fam";
+		}
+		else if (statement.indexOf("Java GC sucks") >= 0)
+		{
+			response = "If it worked properly, half of the programs would terminate directly after initialization.";
+		}
+		else
+		
+		{
+			response = getRandomResponse();
+		}
+		return response;
+	}
+
+	/**
+	 * Pick a default response to use if nothing else fits.
+	 * @return a non-committal string
+	 */
+	private String getRandomResponse()
+	{
+		final int NUMBER_OF_RESPONSES = 6;
+		double r = Math.random();
+		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+		String response = "";
+		
+		if (whichResponse == 0)
+		{
+			response = "Interesting, tell me more.";
+		}
+		else if (whichResponse == 1)
+		{
+			response = "Hmmm.";
+		}
+		else if (whichResponse == 2)
+		{
+			response = "Do you really think so?";
+		}
+		else if (whichResponse == 3)
+		{
+			response = "You don't say.";
+		}
+		else if (whichResponse == 4)
+		{
+			response = "Cool story bro.";
+		}
+		else if(whichResponse == 5)
+		{
+			response = "I'm sure that was funny when it happened."
+		}
+		return response;
+	}
+}
