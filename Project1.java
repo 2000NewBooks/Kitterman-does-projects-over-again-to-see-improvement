6c36ef9f1549d43cd8d731124374808dffb4d67b
Andrew Kitterman <taupitheta@gmail.com>
2016-05-16 18:44:25 -0600

    Solution to project 1

---
+++ Project1.java
@@ -0,0 +1,25 @@
+public class Project1
+{
+  int i;
+  String str;
+  boolean b;
+  
+  public Project1()
+  {
+    i = 5;
+    str = "Hello World!";
+    b = false;
+  }
+  public void setI(int x)
+  {
+    i = x;
+  }
+  public int getString()
+  {
+    return str;
+  }
+  public boolean getB()
+  {
+    return b;
+  }
+}
