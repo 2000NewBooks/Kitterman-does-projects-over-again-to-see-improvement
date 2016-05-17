93d18bb1351835c0efd5cd8e28d83d17ae8aac9f
Andrew Kitterman <taupitheta@gmail.com>
2016-05-17 07:39:08 -0600

    Point should be done.

--- Point.java
+++ Point.java
@@ -1,1 +1,26 @@
-
+public class Point
+{
+  double x;
+  double y;
+  public Point(double x, double y)
+  {
+      
+    this.x = x;
+    this.y = y;
+  }
+  public double getX()
+  {
+      return x;
+    
+  }
+  public double getY()
+  {
+    return y;
+  }
+  public String toString()
+  {
+    return "(" + x + ", " + y + ")";
+  }
+  
+  
+}
