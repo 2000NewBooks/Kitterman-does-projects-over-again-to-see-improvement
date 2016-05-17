d41f5956498cc9b44e29329dc53245d441e23350
Andrew Kitterman <taupitheta@gmail.com>
2016-05-17 08:58:45 -0600

    Polygon.java is done

--- Polygon.java
+++ Polygon.java
@@ -1,1 +1,21 @@
 //Project 4
+public class Polygon extends Shape {
+    private Line[] sides;
+    public Polygon(Line[] sides){
+        this.sides = sides;
+    }
+
+    public double getArea(){
+        return 2.5;
+    }
+
+    public double getPerimeter(){
+        double perimeter = 0;
+        for (Line side : sides)
+        { perimeter = perimeter + side.getLength();
+        }
+        return perimeter;
+    }	
+
+    
+}
