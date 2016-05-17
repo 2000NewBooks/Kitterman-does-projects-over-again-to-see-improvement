2b2eb38b7db60e6f47f799fd4d6e0799ae94c8d0
Andrew Kitterman <taupitheta@gmail.com>
2016-05-16 18:47:15 -0600

    Added Javadoc.

--- Project1.java
+++ Project1.java
@@ -3,21 +3,36 @@ public class Project1
   int i;
   String str;
   boolean b;
-  
+  /**
+   * non-default constructor
+   * Initializes all state variables
+   */
   public Project1()
   {
     i = 5;
     str = "Hello World!";
     b = false;
   }
+  /**
+   *This sets the integer i to whatever
+   * you want it to be
+   */
   public void setI(int x)
   {
     i = x;
   }
+  /**
+   *returns the String we initialized.
+   * Should return Hello World!
+   */
   public String getString()
   {
     return str;
   }
+  /**
+   * Gets the b value. 
+   * Should return false.
+   */
   public boolean getB()
   {
     return b;
